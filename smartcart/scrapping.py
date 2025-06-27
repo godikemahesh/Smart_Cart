b574a9e1b7f79d40820819beacf57f5e10e7a0cf
Mahesh godike <69629676+godikemahesh@users.noreply.github.com>
2025-06-27 19:25:33 +0530

    Update scrapping.py

--- smartcart/scrapping.py
+++ smartcart/scrapping.py
@@ -15,10 +15,6 @@ HEADERS = {
 
 
 
-#HEADERS = {
-#     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
-# }
-
 
 def scrape_shopclues(query, max_results=1):
     url = f"https://www.shopclues.com/search?q={query.replace(' ', '+')}"
